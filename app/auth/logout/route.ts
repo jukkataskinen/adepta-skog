de9e03bd33eec48c641875a35fe47d2a83cad01c
jukkataskinen <jukka.taskinen@adepta.fi>
2026-04-01 09:16:49 +0300

    fix auth0 v4 route handlers

--- app/auth/logout/route.ts
+++ app/auth/logout/route.ts
@@ -2,5 +2,5 @@ import { auth0 } from '../../../lib/auth0'
 import { NextRequest } from 'next/server'
 
 export async function GET(request: NextRequest) {
-  return auth0.handlers.logout(request)
+  return auth0.middleware(request)
 }
